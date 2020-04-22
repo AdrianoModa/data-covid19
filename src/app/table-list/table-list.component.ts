bac433540abb8fa0fe9cab0f28e2e20efbf9fe41
adriano.moda <adriano.moda@COMETA.INTRANET>
2020-04-22 13:19:00 -0300

    propriedades tabela global e logo como home

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -13,6 +13,14 @@ export class TableListComponent implements OnInit {
   searchTerm: string
 
   dataCovid: Global[] = []
+  dataCovidGlobal: Global[] = []
+  newConfirmed: number;
+  totalConfirmed: number;
+  newDeaths: number;
+  totalDeaths: number;
+  newRecovered: number;
+  totalRecovered: number;
+
 
   constructor(
     private dataCovidService: DataCovidService
@@ -20,13 +28,34 @@ export class TableListComponent implements OnInit {
 
   ngOnInit() {
     this.listData()
+    this.listGlobalData()
+    this.listGlobalDataProperties()
   }
 
-  listData(){
+  listData() {
     this.dataCovidService.getAllData()
       .toPromise()
       .then(response => this.dataCovid = response)
   }
+  listGlobalData() {
+    this.dataCovidService.getAllData()
+      .toPromise()
+      .then(response => this.dataCovidGlobal = response)
+  }
 
-  
+  listGlobalDataProperties() {
+    this.dataCovidService.getAllData()
+      .toPromise()
+      .then(response => this.dataCovidGlobal = response.Global)
+      .then(() => {
+        this.newConfirmed = this.dataCovidGlobal['NewConfirmed'],
+        this.totalConfirmed = this.dataCovidGlobal['TotalConfirmed'],
+        this.newDeaths = this.dataCovidGlobal['NewDeaths'],
+        this.totalDeaths = this.dataCovidGlobal['TotalDeaths'],
+        this.newRecovered = this.dataCovidGlobal['NewRecovered'],
+        this.totalRecovered = this.dataCovidGlobal['TotalRecovered']
+      })
+  }
+
+
 }
